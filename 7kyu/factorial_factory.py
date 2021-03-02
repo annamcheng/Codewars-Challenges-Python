351575b0aa4211af725af74d36c193027e80bd4d
Anna Cheng <acheng0086@gmail.com>
2021-03-01 20:04:22 -0500

    factorial factory challenge completed

---
+++ 7kyu/factorial_factory.py
@@ -0,0 +1,17 @@
+"""
+Factorial Factory
+https://www.codewars.com/kata/528e95af53dcdb40b5000171/train/python
+
+In mathematics, the factorial of integer 'n' is written as 'n!'. It is equal to the product of n and every integer preceding it. For example: 5! = 1 x 2 x 3 x 4 x 5 = 120
+
+Your mission is simple: write a function that takes an integer 'n' and returns 'n!'.
+
+You are guaranteed an integer argument. For any values outside the positive range, return null, nil or None .
+
+Note: 0! is always equal to 1. Negative values should return null;
+"""
+
+def factorial(n):
+     return 1 if n == 0 else None if n < 0 else n * factorial(n-1)
+
+print(factorial(5))
